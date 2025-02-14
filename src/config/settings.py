d4ab25337960ac19989be822b07ebac6f003cc3c
lx <2323658175@qq.com>
2025-02-14 21:08:10 +0800

    add planner and test

--- src/config/settings.py
+++ src/config/settings.py
@@ -7,3 +7,13 @@ DOUBAO_SETTINGS = {
     "base_url": "https://ark.cn-beijing.volces.com/api/v3",
     "model": "ep-20250213235435-rpbp7"
 }
+
+_AGENT_SETTING_AIGIRLFRIEND = {
+    "name": "苏瑶",
+    "age": 20,
+    "gender": "女",
+    "occupation": "学生",
+    "personality": "cute"
+}
+
+AGENT_SETTINGS = _AGENT_SETTING_AIGIRLFRIEND
---
+++ tests/plan_test.py
@@ -0,0 +1,30 @@
+import sys
+import os
+from datetime import datetime
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from src.agent.plan import Planner
+from src.agent.memory import MemoryItem
+
+
+def test_create_plan():
+    planner = Planner()
+    history = [
+        MemoryItem(user_input="你好啊",
+                   ai_response="你好呀，见到你真开心~",
+                   timestamp=datetime.now(),
+                   actions=[]),
+        MemoryItem(user_input="今晚月色真美",
+                   ai_response="风也温柔",
+                   timestamp=datetime.now(),
+                   actions=[])
+    ]
+
+    user_message = "今天天气真好"
+    plan = planner.create_plan(user_message, history)
+    assert plan
+    print(plan)
+
+
+if __name__ == '__main__':
+    test_create_plan()
