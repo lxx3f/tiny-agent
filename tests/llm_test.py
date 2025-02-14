63e2bb58d287a7ea068b379619fe10cac995dd3c
lx <2323658175@qq.com>
2025-02-14 21:07:33 +0800

    test llm

--- tests/llm_test.py
+++ tests/llm_test.py
@@ -8,7 +8,15 @@ from src.llm.base import DouBaoService
 
 def test_doubao_call():
     llm_service = DouBaoService()
-    prompt = "故障机器人是不是尖塔最强角色？"
+    prompt = "一句话回答：故障机器人是不是尖塔最强角色？"
+    response = llm_service.call(prompt)
+    assert response
+    print(response)
+
+
+def test_a_call():
+    llm_service = DouBaoService()
+    prompt = input()
     response = llm_service.call(prompt)
     assert response
     print(response)
@@ -16,3 +24,4 @@ def test_doubao_call():
 
 if __name__ == "__main__":
     test_doubao_call()
+    # test_a_call()
